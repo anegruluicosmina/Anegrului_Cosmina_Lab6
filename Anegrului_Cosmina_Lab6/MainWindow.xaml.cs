80c6d1978aeb248a1ace8c0c06bdbc84034eceef
MARIA-COSMINA ANEGRULUI <maria.anegrului@stud.ubbcluj.ro>
2020-11-19 16:38:56 +0200

    Add project files.

---
+++ Anegrului_Cosmina_Lab6/MainWindow.xaml.cs
@@ -0,0 +1,255 @@
+﻿using AutoLotModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Anegrului_Cosmina_Lab6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    enum ActionState
+    {
+        New,
+        Edit,
+        Delete,
+        Nothing
+    }
+    public partial class MainWindow : Window
+    {
+
+        ActionState action = ActionState.Nothing;
+        AutoLotEntitiesModel ctx = new AutoLotEntitiesModel();
+        CollectionViewSource customerViewSource;
+        CollectionViewSource customerOrderViewSource;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            customerViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customerViewSource")));
+            customerViewSource.Source = ctx.Customer.Local;
+            customerOrderViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customerOrdersViewSource")));
+            //customerOrderViewSource.Source = ctx.Order.Local;
+            ctx.Customer.Load();
+            ctx.Order.Load();
+            ctx.Inventory.Load();
+            cmbCustomer.ItemsSource = ctx.Customer.Local;
+            //cmbCustomer.DisplayMemberPath = "FirstName";
+            cmbCustomer.SelectedValuePath = "CustId";
+            cmbInventory.ItemsSource = ctx.Inventory.Local;
+            //cmbInventory.DisplayMemberPath = "Make";
+            cmbInventory.SelectedValuePath = "CarId";
+            System.Windows.Data.CollectionViewSource inventoryViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("inventoryViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // inventoryViewSource.Source = [generic data source]
+            System.Windows.Data.CollectionViewSource orderViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("orderViewSource")));
+            // Load data by setting the CollectionViewSource.Source property:
+            // orderViewSource.Source = [generic data source]
+            BindDataGrid();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            Customer customer = null;
+            if (action == ActionState.New)
+            {
+                try
+                {
+                    customer = new Customer()
+                    {
+                        FirstName = firtsNameTextBox.Text.Trim(),
+                        LastName = lastNameTextBox.Text.Trim()
+                    };
+
+                    ctx.Customer.Add(customer);
+                    customerViewSource.View.Refresh();
+
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                btnNew.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                btnCancel.IsEnabled = false;
+                btnNext.IsEnabled = true;
+                lastNameTextBox.IsEnabled = false;
+                firtsNameTextBox.IsEnabled = false;
+            }
+            else if (action == ActionState.Edit)
+            {
+                try
+                {
+                    customer = (Customer)customerDataGrid.SelectedItem;
+                    customer.FirstName = firtsNameTextBox.Text.Trim();
+                    customer.LastName = lastNameTextBox.Text.Trim();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                customerViewSource.View.Refresh();
+                customerViewSource.View.MoveCurrentTo(customer);
+                btnNew.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                btnDelete.IsEnabled = true;
+                btnCancel.IsEnabled = false;
+                btnPreious.IsEnabled = true;
+                btnNext.IsEnabled = true;
+                lastNameTextBox.IsEnabled = false;
+                firtsNameTextBox.IsEnabled = false;
+
+            }
+            else if (action == ActionState.Delete)
+            {
+                try
+                {
+                    customer = (Customer)customerDataGrid.SelectedItem;
+                    ctx.Customer.Remove(customer);
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                btnNew.IsEnabled = false;
+                btnEdit.IsEnabled = false;
+                btnDelete.IsEnabled = false;
+                btnSave.IsEnabled = true;
+                btnCancel.IsEnabled = true;
+                btnPreious.IsEnabled = false;
+                btnNext.IsEnabled = false;
+                lastNameTextBox.IsEnabled = false;
+                firtsNameTextBox.IsEnabled = false;
+            }
+
+        }
+
+        private void btnPreious_Click(object sender, RoutedEventArgs e)
+        {
+            customerViewSource.View.MoveCurrentToPrevious();
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            customerViewSource.View.MoveCurrentToNext();
+        }
+
+        private void btnPreiousO_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSaveO_Click(object sender, RoutedEventArgs e)
+        {
+            Order order = null;
+            if (action == ActionState.New)
+            {
+                try
+                {
+                    Customer customer = (Customer)cmbCustomer.SelectedItem;
+                    Inventory inventory = (Inventory)cmbInventory.SelectedItem;
+                    order = new Order()
+                    {
+
+                        CustId = customer.CustId,
+                        CarId = inventory.CarId
+                    };
+                    ctx.Order.Add(order);
+                    customerOrderViewSource.View.Refresh();
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                btnNew.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                btnCancel.IsEnabled = false;
+                btnNext.IsEnabled = true;
+                lastNameTextBox.IsEnabled = false;
+                firtsNameTextBox.IsEnabled = false;
+            }
+            if (action == ActionState.Edit)
+            {
+                dynamic selectedOrder = ordersDataGrid.SelectedItem;
+                try
+                {
+                    int curr_id = selectedOrder.OrderId;
+                    var editedOrder = ctx.Order.FirstOrDefault(s => s.OrderId == curr_id);
+                    if (editedOrder != null)
+                    {
+                        editedOrder.CustId = Int32.Parse(cmbCustomer.SelectedValue.ToString());
+                        editedOrder.CarId = Convert.ToInt32(cmbInventory.SelectedValue.ToString());
+                        ctx.SaveChanges();
+                    }
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                BindDataGrid();
+                // pozitionarea pe item-ul curent
+                customerViewSource.View.MoveCurrentTo(selectedOrder);
+            }
+            else if (action == ActionState.Delete)
+            {
+                try
+                {
+                    dynamic selectedOrder = ordersDataGrid.SelectedItem;
+                    int curr_id = selectedOrder.OrderId;
+                    var deletedOrder = ctx.Order.FirstOrDefault(s => s.OrderId == curr_id);
+                    if (deletedOrder != null)
+                    {
+                        ctx.Order.Remove(deletedOrder);
+                        ctx.SaveChanges();
+                        MessageBox.Show("Order Deleted Successfully", "Message");
+                        BindDataGrid();
+                    }
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+           
+        }
+        private void BindDataGrid()
+        {
+            var queryOrder = from ord in ctx.Order
+                             join cust in ctx.Customer on ord.CustId equals
+                             cust.CustId
+                             join inv in ctx.Inventory on ord.CarId
+                             equals inv.CarId
+                             select new
+                             {
+                                 ord.OrderId,
+                                 ord.CarId,
+                                 ord.CustId,
+                                 cust.FirstName,
+                                 cust.LastName,
+                                 inv.Make,
+                                 inv.Color
+                             };
+            customerOrderViewSource.Source = queryOrder.ToList();
+        }
+    }
+
+}
---
+++ Anegrului_Cosmina_Lab6/Validation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Anegrului_Cosmina_Lab6
+{
+    //validator pentru camp required
+    public class StringNotEmpty : ValidationRule
+    {
+        //mostenim din clasa ValidationRule
+        //suprascriem metoda Validate ce returneaza un
+        //ValidationResult
+        public override System.Windows.Controls.ValidationResult Validate(object value, System.Globalization.CultureInfo cultureinfo)
+        {
+            string aString = value.ToString();
+            if (aString == "")
+                return new System.Windows.Controls.ValidationResult(false, "String cannot be empty");
+            return new System.Windows.Controls.ValidationResult(true, null);
+        }
+    }
+    //validator pentru lungime minima a string-ului
+    public class StringMinLengthValidator : ValidationRule
+    {
+        public override System.Windows.Controls.ValidationResult Validate(object value, System.Globalization.CultureInfo cultureinfo)
+        {
+            string aString = value.ToString();
+            if (aString.Length < 3)
+                return new System.Windows.Controls.ValidationResult(false, "String must have at least 3  characters!");
+        return new System.Windows.Controls.ValidationResult(true, null);
+        }
+    }
+}
